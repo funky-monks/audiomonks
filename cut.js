2bfdb9a05fe741c94e2bf90b2c63ca4ce51a26e2
Brandon <brandonlbmx98@gmail.com>
2022-05-21 18:54:32 -0500

    finished cut functionality

--- cut.js
+++ cut.js
@@ -2,6 +2,7 @@ const MP3Cutter = require('mp3-cutter');
 const fs = require('fs');
 const mp3Duration = require('mp3-duration');
 const listDirectory = require('./listDirectory');
+const mm = require('music-metadata');
 
 const difficulties = ['hard', 'normal', 'easy']
 
@@ -24,31 +25,28 @@ async function listDirectoryMP3(pathOfAlbum) {
 }
 
 // get duration in seconds minus and cut first and last 5% (song might be fading in/out)
-function getDuration(filePath) {
-    const songDuration = mp3Duration(filePath, (err, duration) => {
-        if (err) return console.log(err.message);
+async function getDurationObject(songMetadataDuration) {
+    const songDuration = await songMetadataDuration
 
-        const percentFade = 5
-        const percentageFinalSeconds = Math.round((duration / 100) * percentFade)
+    const percentFade = 5
+    const percentageFinalSeconds = await Math.round((songDuration / 100) * percentFade)
 
-        const songDurationObject = {
-            startTime: percentageFinalSeconds,
-            finalTime: Math.round(duration - percentageFinalSeconds)
-        }
-        return songDurationObject
-    });
-    console.log(songDuration)
+    const songDurationObject = await {
+        startTime: percentageFinalSeconds,
+        finalTime: Math.round(songDuration - percentageFinalSeconds)
+    }
+    return songDurationObject
 }
 
 async function cutFileMp3(source, targetFile, startTime, finalTime) {
     try {
         MP3Cutter.cut({
             src: source,
-            target: targetFile,
+            target: targetFile + '.mp3',
             start: startTime,
             end: finalTime
         });
-        console.log(`Success cutting song from ${source}`)
+        // console.log(`Success cutting song from ${source}`)
     } catch (error) {
         console.log(error)
         return null
@@ -59,26 +57,99 @@ async function cutFileMp3(source, targetFile, startTime, finalTime) {
 const albumName = 'Unlimited Love'
 const albumPath = `./music/${albumName}`
 
-createDifficultyDir(albumName).then(e => {
-    listDirectoryMP3(albumPath).then(files => {
-        files.forEach(filepath => {
-            getDuration(filepath).then(songObject => {
-                console.log(songObject)
-            })
-            // difficulties.forEach(difficulty => {
-            //     targetPath = `./music/${difficulty}/${songDuration.startTime}-${songDuration.finalTime}`
-            //     // cutFileMp3(filepath, targetPath, songDuration.startTime, songDuration.finalTime).then(e => {
+async function parseAudioMetadata(filePath) {
+    const metadata = await mm.parseFile(filePath);
+    return metadata
+}
 
-            //     // })
-            // });
-        });
+async function mainCut() {
+    createDifficultyDir(albumName).then(() => {
+        listDirectoryMP3(albumPath).then(files => {
+            files.forEach(filepath => {
+                parseAudioMetadata(filepath).then(songMetadata => {
+                    const artist = songMetadata.common.artist
+                    const album = songMetadata.common.album
+                    const title = songMetadata.common.title
+                    const duration = songMetadata.format.duration
+
+                    getDurationObject(duration).then(songDurationObject => {
+                        const startTime = songDurationObject.startTime
+                        const finalTime = songDurationObject.finalTime
+
+
+
+                        difficulties.forEach(difficulty => {
+                            let durationOfDifficulty = 1
+                            if (difficulty === 'normal') {
+                                durationOfDifficulty = 2.5
+                            } else if (difficulty === 'easy') {
+                                durationOfDifficulty = 5
+                            }
+                            console.log(`cutting songs into ${difficulty} difficulty:`)
+                            for (let index = startTime; index < finalTime; index += durationOfDifficulty) {
+                                const targetPath = `${albumPath}/${difficulty}/${title}-${index}-${index + durationOfDifficulty}`
+                                cutFileMp3(filepath, targetPath, index, index + durationOfDifficulty).then(() => {
+                                })
+                                
+                            }
+                            console.log('done.')
+
+                        });
+                    })
+                })
+            });
+        })
     })
+}
+
+mainCut().then(() => {
+
 })
 
-// MP3Cutter.cut({
-//     src: './music/Unlimited Love/01 Black Summer.mp3',
-//     target: './music/Unlimited Love/hard/ds323d.mp3',
-//     start: 121,
-//     end: 122
-// });
-
+// metadata object example:
+// {
+//     format: {
+//       tagTypes: [ 'ID3v2.3', 'ID3v1' ],
+//       trackInfo: [],
+//       lossless: false,
+//       container: 'MPEG',
+//       codec: 'MPEG 1 Layer 3',
+//       sampleRate: 44100,
+//       numberOfChannels: 2,
+//       bitrate: 256000,
+//       codecProfile: 'CBR',
+//       tool: 'LAME 3.99.5',
+//       trackPeakLevel: undefined,
+//       trackGain: -8.7,
+//       duration: 331.52
+//     },
+//     native: {
+//       'ID3v2.3': [
+//         [Object], [Object],
+//         [Object], [Object],
+//         [Object], [Object],
+//         [Object], [Object],
+//         [Object], [Object],
+//         [Object], [Object],
+//         [Object]
+//       ],
+//       ID3v1: [ [Object], [Object], [Object], [Object], [Object], [Object] ]
+//     },
+//     quality: { warnings: [] },
+//     common: {
+//       track: { no: 16, of: null },
+//       disk: { no: 1, of: null },
+//       movementIndex: {},
+//       album: 'Unlimited Love',
+//       albumartist: 'Red Hot Chili Peppers',
+//       artists: [ 'Red Hot Chili Peppers' ],
+//       artist: 'Red Hot Chili Peppers',
+//       bpm: 0,
+//       year: 2022,
+//       encodedby: 'Switch Trial Version © NCH Software',
+//       encodersettings: 'Muziek 1.1.6.37',
+//       genre: [ 'Rock' ],
+//       title: 'The Heavy Wing',
+//       picture: [ [Object] ]
+//     }
+//   }
